ef7120accc9a6b05e699add6c368cc0a33012c75
HavinVh <167575800+HavinVh@users.noreply.github.com>
2025-07-19 23:39:53 +0530

    Time: 964 ms (66.20%) | Memory: 20.7 MB (86.04%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,36 +1,36 @@
 class Solution(object):
     def sortArray(self, nums):
         
-        def merger(arr,L,M,R):
-            left,right=arr[L:M+1],arr[M+1:R+1]
+        def merger(nums,L,M,R):
+            left,right=nums[L:M+1],nums[M+1:R+1]
             i,j,k=L,0,0
             while j<len(left) and k<len(right):
                 if left[j] <= right[k]:
-                    arr[i]=left[j]
+                    nums[i]=left[j]
                     j+=1
                 else:
-                    arr[i]=right[k]
+                    nums[i]=right[k]
                     k+=1
                 i+=1
             #when one list is exhausted
             while j<len(left):
-                arr[i]=left[j]
+                nums[i]=left[j]
                 j+=1
                 i+=1
             while j<len(right):
-                arr[i]=right[k]
+                nums[i]=right[k]
                 k+=1
                 i+=1
 
 
-        def mergesort(arr,l,r):
+        def mergesort(nums,l,r):
             if l==r:
-                return arr
+                return nums
             m=(l+r)//2
-            mergesort(arr,l,m)
-            mergesort(arr,m+1,r)
-            merger(arr,l,m,r)
-            return arr
+            mergesort(nums,l,m)
+            mergesort(nums,m+1,r)
+            merger(nums,l,m,r)
+            return nums
         return mergesort(nums,0,len(nums)-1)
         #copied
 
