3e06eb13fb4dab309dd83b41d9aee52778abcae8
HavinVh <167575800+HavinVh@users.noreply.github.com>
2025-07-22 01:20:25 +0530

    Time: 4 ms (11.76%) | Memory: 12.4 MB (85.62%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -16,18 +16,5 @@ class Solution(object):
                     mat[i][j],mat[i][len(mat)-j-1]= mat[i][len(mat)-j-1],mat[i][j]
             if mat==target:
                 return True
-        mat=mat1
-        k=0
-        for k in range(4):
-            for j in range(0,len(mat)):#transpose of the matrix
-                for i in range(j,len(mat)):
-                    mat[j][i],mat[i][j]=mat[i][j],mat[j][i]
-            for j in range(len(mat)):
-                for i in range((len(mat))//2):
-                    #two pointer
-                    mat[j][i],mat[j][len(mat)-i-1]= mat[j][len(mat)-i-1],mat[j][i]
-        if mat==target:
-            return True
         else:
-            return False
-
+            return False      
