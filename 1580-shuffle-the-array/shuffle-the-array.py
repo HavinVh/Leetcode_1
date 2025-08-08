9bd2ff1f21478785597873483bd2fa70d6f5f55a
HavinVh <167575800+HavinVh@users.noreply.github.com>
2025-08-08 21:51:30 +0530

    Time: 41 ms (46.13%) | Memory: 12.6 MB (43.87%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -5,17 +5,8 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        j=0
-        i=0
-        k=0
-        nums1=nums[:n]
-        nums2=nums[n:]
-        while k<len(nums):
-            if k%2==0:
-                nums[k]=nums1[i]
-                i+=1
-            else:
-                nums[k]=nums2[j]
-                j+=1
-            k+=1
-        return nums
+        res=[]
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[i+n])
+        return res
